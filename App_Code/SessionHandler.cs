71d5a6071a170037999c79cbf977a36a90a937f7
Daniel Huwiler <daniel>
2017-12-06 11:00:54 +0100

    startProj

---
+++ App_Code/SessionHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Zusammenfassungsbeschreibung für SessionHandler
+/// </summary>
+public class SessionHandler
+{
+
+	private static SessionHandler instance;
+
+	public static SessionHandler getInstance()
+	{
+		if (instance == null)
+		{
+			instance = new SessionHandler();
+		}
+		return instance;
+	}
+
+	private String userToken;
+
+	private SessionHandler()
+	{
+		
+	}
+
+	public bool loginUser(String username, String password)
+	{
+		if (checkUserLogin(username, password)) {
+			userToken = generateUserToken();
+		}
+		return true;
+	}
+
+	private String generateUserToken()
+	{
+		return "userLoggedIn";
+	}
+
+	public String getUserToken()
+	{
+		return userToken;
+	}
+
+	private bool checkUserLogin(String username, String password)
+	{
+		return (username == "admin" && password == "eli");
+	}
+
+}
---
+++ LoginPage.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+	protected void Page_Load(object sender, EventArgs e)
+	{
+		if(IsPostBack)
+		{
+			handleLogin();
+		}
+		handleSession();
+	}
+
+	/**
+	 * Redirect to User-Section if User already logged in 
+	 **/
+	private void handleSession()
+	{
+		String sessionKey = (String)Session["key"];
+		if (sessionKey != null)
+		{
+			redirectToUserSection();
+		}
+	}
+
+	private void redirectToUserSection()
+	{
+		Response.Redirect("MainMenu.aspx");
+	}
+
+	private void handleLogin()
+	{
+		String username = UsernameField.Text;
+		String password = PasswordField.Text;
+		if (SessionHandler.getInstance().loginUser(username, password))
+		{
+			Session["key"] = SessionHandler.getInstance().getUserToken();
+		}
+
+	}
+}
